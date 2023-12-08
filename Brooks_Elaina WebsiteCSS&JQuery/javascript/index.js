e352a9d003eb850882ff4f31b57e3d3708125d6a
ElainaB1203 <145591959+ElainaB1203@users.noreply.github.com>
2023-12-08 09:17:09 -0700

    Website Deploy
    Added is index and all website assets

---
+++ Brooks_Elaina WebsiteCSS&JQuery/javascript/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#mobile-button').click(function() {
+        $('#mobile-nav').fadeIn('slow');
+    });
+    $('#close-mobile-button').click(function(){
+        $('#mobile-nav').fadeOut();
+    });
+});
+$(document).ready(function() {
+    $('#mobile-button').click(function() {
+        $('#mobile-nav').fadeIn('slow');
+    });
+    $('#close-mobile-button').click(function(){
+        $('#mobile-nav').fadeOut();
+    });
+});
